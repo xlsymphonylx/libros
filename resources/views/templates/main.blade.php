c0fbaa75d28db9afbd048e15a6e26f5fd6fb6df5
xlsymphonylx <xlsymphonylx@gmail.com>
2022-08-06 11:39:15 -0600

    implementación de Bootstrap

---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Libros</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Libros</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link " href="/">Inicio</a>
+                    <a class="nav-link " href="/">Libros</a>
+                    <a class="nav-link " href="/">Editoriales</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+</body>
+
+</html>
