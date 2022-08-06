d26bde5a70a9e9503bf2ddee4b06f63422fae8cb
xlsymphonylx <xlsymphonylx@gmail.com>
2022-08-06 11:52:20 -0600

    modelos

---
+++ app/Models/Editorial.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Editorial extends Model
+{
+    use HasFactory;
+    protected $table = "editorial";
+    protected $guarded = ['id'];
+    public $timestamps = false;
+}
