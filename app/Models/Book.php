2a9366c4993a0d67c168d9fca445181aae8b2ecd
xlsymphonylx <xlsymphonylx@gmail.com>
2022-08-06 11:52:40 -0600

    modelos2

---
+++ app/Models/Book.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+    protected $table = "book";
+    protected $guarded = ['id'];
+    public $timestamps = false;
+
+    public function editorial()
+    {
+        return $this->hasOne(Editorial::class, 'id', 'editorial_id');
+    }
+}
